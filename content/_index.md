3219dc9ff335fce859edfdc5164700b5fcb48ddf
Matthias Fleckenstein <magicmaze@t-online.de>
2025-11-16 17:29:51 -0500

    Initial commit

---
+++ content/_index.md
@@ -0,0 +1,21 @@
++++
+title = "FINC672 - Financial Data Analytics"
+type = "home"
++++
+
+This is your new home page.
+
+
+
+<details>
+  <summary style="cursor:pointer; font-weight:bold;">
+    📄 Show Syllabus
+  </summary>
+
+  <div style="margin-top:1rem;">
+    <iframe
+      src="./syllabus/FINC462-662_Syllabus_Spring2025.pdf"
+      style="width:100%; height:700px; border:none;">
+    </iframe>
+  </div>
+</details>
---
+++ docs/searchindex.en.js
@@ -0,0 +1,74 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "FINC672 - Financial Data Analytics \u003e Assignments",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Assignment 01",
+    "uri": "/FINC672-SP2026/assignments/assignment_01/index.html"
+  },
+  {
+    "breadcrumb": "FINC672 - Financial Data Analytics \u003e Lectures",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Lecture 01",
+    "uri": "/FINC672-SP2026/lectures/lecture_01/index.html"
+  },
+  {
+    "breadcrumb": "FINC672 - Financial Data Analytics",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Lectures",
+    "uri": "/FINC672-SP2026/lectures/index.html"
+  },
+  {
+    "breadcrumb": "FINC672 - Financial Data Analytics \u003e Assignments",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Assignment 02",
+    "uri": "/FINC672-SP2026/assignments/assignment_02/index.html"
+  },
+  {
+    "breadcrumb": "FINC672 - Financial Data Analytics",
+    "content": "Assignment 01 Assignment 02",
+    "description": "Assignment 01 Assignment 02",
+    "tags": [],
+    "title": "Assignments",
+    "uri": "/FINC672-SP2026/assignments/index.html"
+  },
+  {
+    "breadcrumb": "FINC672 - Financial Data Analytics \u003e Lectures",
+    "content": "Lectures\rLecture 1 Slides",
+    "description": "Lectures\rLecture 1 Slides",
+    "tags": [],
+    "title": "Lecture 02",
+    "uri": "/FINC672-SP2026/lectures/lecture_02/index.html"
+  },
+  {
+    "breadcrumb": "FINC672 - Financial Data Analytics",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/FINC672-SP2026/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "This is your new home page.\n📄 Show Syllabus",
+    "description": "This is your new home page.\n📄 Show Syllabus",
+    "tags": [],
+    "title": "FINC672 - Financial Data Analytics",
+    "uri": "/FINC672-SP2026/index.html"
+  },
+  {
+    "breadcrumb": "FINC672 - Financial Data Analytics",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/FINC672-SP2026/tags/index.html"
+  }
+]
